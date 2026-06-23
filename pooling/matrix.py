4e8b4af8919df927330a9db480b8215a5cd92431
Wieland Glende <wieland.glende@deutschebahn.com>
2026-06-23 22:10:25 +0200

    just formatting to get stash running

--- pooling/matrix.py
+++ pooling/matrix.py
@@ -5,9 +5,12 @@ from pooling_types import CustomerJourney
 ORS_MATRIX_URL = "https://api.openrouteservice.org/v2/matrix/driving-car"
 
 
-def get_distance_matrix_and_destination_mapping(source_lon: float, source_lat: float,
-                                                customer_journeys: list[CustomerJourney],
-                                                api_token: str) -> list[list[float]]:
+def get_distance_matrix_and_destination_mapping(
+    source_lon: float,
+    source_lat: float,
+    customer_journeys: list[CustomerJourney],
+    api_token: str,
+) -> list[list[float]]:
     destination_coordinates = set()
     destination_names = set()
     for journey in customer_journeys:
@@ -18,7 +21,10 @@ def get_distance_matrix_and_destination_mapping(source_lon: float, source_lat: f
         destination_coordinates.add(coordinates)
         destination_names.add(journey["destination_name"])
 
-    locations = [[source_lon, source_lat] + [coordinates] for coordinates in destination_coordinates]
+    locations = [
+        [source_lon, source_lat] + [coordinates]
+        for coordinates in destination_coordinates
+    ]
 
     headers = {
         "Authorization": f"Bearer {api_token}",
